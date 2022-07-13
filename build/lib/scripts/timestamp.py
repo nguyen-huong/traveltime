68816d5f8b2188493b363250a2ace8dc4d5ae389
nguyen-huong <mhuong1703@gmail.com>
2022-07-13 00:13:02 -0700

    dist

--- build/lib/scripts/timestamp.py
+++ build/lib/scripts/timestamp.py
@@ -7,13 +7,9 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from datetime import datetime
 from geopy.distance import geodesic
-from math import atan
-import math
-from tasks import *
-from timestamp import *
-from map import *
 
-def map_arrow(df):
+
+def get_timestamp(df):
     """
     Get the dataframe with timestamps and return a map with permanent timestamps
     """
@@ -31,36 +27,15 @@ def map_arrow(df):
     # change map center with the avg of the data
     map = folium.Map([data.Lat.mean(), data.Long.mean()], zoom_start=12)
 
-    # #add arrow
-    # la1 = lat[0]
-    # lo1 = long[0]
-    # la2 = lat[200]
-    # lat2 = lat[-1]
-    # lo2 = long[200]
-    # lon2 = long[-1]
-    # coordinates=[((la1), (lo1)),((la2), (lo2))]
-    # latitude_difference = lat2-la1
-    # longitude_difference=lon2-lo1
-
-    # map = folium.Map([data.Lat.mean(), data.Long.mean()], zoom_start=12)
-    # aline=folium.PolyLine(locations=coordinates,weight=2,color = 'blue')
-    # map.add_child(aline)
-
-    # folium.RegularPolygonMarker(location=(la2, lo2), fill_color='blue', number_of_sides=3, radius=10, rotation= math.degrees((latitude_difference/longitude_difference))).add_to(map)
-
     # add a marker for each point
     for lt, ln, s, t, dt in zip(lat, long, speed, date, date2):
         feature.add_child(
-            folium.CircleMarker(location=[lt, ln], radius=.7, tooltip=folium.Tooltip(text=dt + ' ' + t),
+            folium.CircleMarker(location=[lt, ln], radius=4, tooltip=folium.Tooltip(text=dt + ' ' + t),
                                 fill_color=coloring(s), color=coloring(s), fill_opacity=0.7))
-
-    # add arrow
-    create_arrow(map)
-    plugins.ScrollZoomToggler().add_to(map)
 
     # call for legend
     map = create_legend(map, 'Speed - MPH',
-                        colors=['red', 'orange', 'yellow', '#60f250'],
+                        colors=['red', 'orange', 'yellow', '#98ebb0'],
                         labels=['0-20', '21-40', '41-55', '55+'])
 
     # time stats
@@ -74,14 +49,6 @@ def map_arrow(df):
     # str time
     tistr1 = str(date_1 + ' ' + time_1)
     tistr2 = str(date_2 + ' ' + time_2)
-
-    # #time differences
-    # if date_2 == date_1:
-    #   timediff = time_2 - time_1
-    # elif date_2 >= date_1:
-    #   daydiff = date_2 - date_1
-    #   timediff = (daydiff * 24) + (time_2 - time_1)
-    # timediff = dt.datetime.strptime(str(time_1), '%H:%M:%S')
 
     t1 = datetime.strptime(str(tistr1), "%m/%d/%y %H:%M:%S")
     t2 = datetime.strptime(str(tistr2), "%m/%d/%y %H:%M:%S")
